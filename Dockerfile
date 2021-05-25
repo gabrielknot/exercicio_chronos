563b8a36f87ba9a246118062ffda4f270fea7efb
gabrielknot <gcftutos@gmail.com>
2021-05-25 12:23:58 -0300

    adding the supervisor

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,18 @@
-FROM alpine as env_builder
-WORKDIR /html
-COPY entryPoint.sh .
-COPY .env .
-RUN chmod +x entryPoint.sh
-ENTRYPOINT ["/entryPoint.sh"]
-FROM nginx 
-COPY --from=env_builder /html /usr/share/nginx/html
-CMD ["nginx", "-g", "daemon off;"]
-#FROM php:7.3-fpm
-#COPY . /usr/share/nginx/blog
-#COPY --from=env_builder .env /usr/share/nginx/blog/.env
-#RUN apt-get update && apt-get install -y \
-#    git \
-#    libzip-dev \
-#    zip \
-#    unzip
-#
-#RUN docker-php-ext-configure zip --with-libzip
-#
-#RUN docker-php-ext-install pdo_mysql zip
-#
-#RUN curl --silent --show-error https://getcomposer.org/installer | php && \
-#    mv composer.phar /usr/local/bin/composer
-#
-#
+FROM php:7.3-fpm
+COPY . /usr/share/nginx/blog
+RUN apt-get update && apt-get install -y \
+    git \
+    libzip-dev \
+    zip \
+    unzip \
+    nginx \
+    supervisor
+
+COPY supervisord.conf /etc/supervisord.conf 
+RUN docker-php-ext-configure zip --with-libzip
+
+RUN docker-php-ext-install pdo_mysql zip
+
+RUN curl --silent --show-error https://getcomposer.org/installer | php && \
+    mv composer.phar /usr/local/bin/composer
+CMD ["/usr/bin/supervisord","-c","/etc/supervisord.conf"]
--- entryPoint.sh
+++ entryPoint.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-set -xe
-: "${DB_ENV_PORT?Need database configuration, environment variables}"
-set -xe
-: "${DB_ENV_DATABASE?Need database configuration, environment variables}"
+# Start the first process
+./my_first_process -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_first_process: $status"
+  exit $status
+fi
 
-set -xe
-: "${DB_ENV_USERNAME?Need database configuration, environment variables}"
-set -xe
-: "${DB_ENV_PASSWORD?Need database configuration, environment variables}"
+# Start the second process
+./my_second_process -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_second_process: $status"
+  exit $status
+fi
 
+# Naive check runs checks once a minute to see if either of the processes exited.
+# This illustrates part of the heavy lifting you need to do if you want to run
+# more than one service in a container. The container exits with an error
+# if it detects that either of the processes has exited.
+# Otherwise it loops forever, waking up every 60 seconds
 
-sed -i "s/DB_ENV_PORT/$DB_ENV_PORT/g" .env
+while sleep 60; do
+  ps aux |grep my_first_process |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  ps aux |grep my_second_process |grep -q -v grep
+  PROCESS_2_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
+done
 
-sed -i "s/DB_ENV_DATABASE/$DB_ENV_DATABASE/g" .env
-
-sed -i "s/DB_ENV_USERNAME/$DB_ENV_USERNAME/g" .env
-
-
-
-sed -i "s/DB_ENV_PASSWORD/$DB_ENV_PASSWORD/g" .env
-
-exec "$@"
